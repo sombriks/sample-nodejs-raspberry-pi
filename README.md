e6c7972cb045e0d1f6ff03d8a67a9929c59ed603
sombriks <sombriks@gmail.com>
2018-11-01 20:50:54 -0300

    let's see it it still works

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # sample-nodejs-raspberry-pi
+
 a few experiments with raspberry pi and nodejs. we want to open doors.
+
+![sample-raspberry-pi.jpg](sample-raspberry-pi.jpg)
+
+![pinout.png](pinout.png)
+
+using (for now)
+
+- ground (09)
+- GPIO04 (07)
+- GPIO17 (11)
+- GPIO27 (13)
+- GPIO22 (15)
+- GPIO23 (16)
--- blink.js
+++ blink.js
@@ -1,23 +1,23 @@
 const Gpio = require('onoff').Gpio;
-const p14 = new Gpio(14,'out');
+const pin = new Gpio(23, 'out');
 
 let blinkInterval;
 
 const blink = _ => {
 	console.log("blink!");
-	if(p14.readSync() == 0)
-		p14.writeSync(1);
+	if (pin.readSync() == 0)
+		pin.writeSync(1);
 	else
-		p14.writeSync(0);
+		pin.writeSync(0);
 }
 
 const endBlink = _ => {
 	console.log("finishing blink...");
 	clearInterval(blinkInterval);
-	p14.writeSync(0);
-	p14.unexport();
+	pin.writeSync(0);
+	pin.unexport();
 	console.log("done");
 }
 
-blinkInterval = setInterval(blink,3000);
-setTimeout(endBlink,15000);
+blinkInterval = setInterval(blink, 3000);
+setTimeout(endBlink, 15000);
